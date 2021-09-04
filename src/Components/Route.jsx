ce4dcc244a087b3bb06f5010415c0ac2c9d3f994
beomki69 <beomki69@gmail.com>
2021-09-04 20:10:05 +0900

    feat:Add api

--- src/Components/Route.jsx
+++ src/Components/Route.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../Routes/Home";
+import Movies from "../Routes/Movies";
+import Search from "../Routes/Search";
+import TV from "../Routes/TV";
 import Header from "./Header";
 
 const RouteComponent = () => {
@@ -9,6 +12,9 @@ const RouteComponent = () => {
       <Header />
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/tv" component={TV} />
+        <Route path="/search" component={Search} />
       </Switch>
     </Router>
   );
---
+++ src/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: "818824afc2c5572a406037e215c9426d",
+    language: "en-US",
+  },
+});
+
+export const movieApi = {
+  topRated: () => api.get("movie/top_rated"),
+  nowPlaying: () => api.get("movie/now_playing"),
+  upComing: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular"),
+  latest: () => api.get("movie/latest"),
+};
